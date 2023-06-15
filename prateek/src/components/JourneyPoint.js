3ad7c20418825e68ee6248bfba07c354ed6a5b93
smolPrateek <prateek.agrawal@skit.ai>
2023-06-15 20:26:43 +0530

    fully fixed journey  point layout

--- prateek/src/components/JourneyPoint.js
+++ prateek/src/components/JourneyPoint.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function JourneyPoint({
   className,
@@ -8,41 +8,62 @@ function JourneyPoint({
   duration,
   links,
 }) {
-  const componentHeight = "72";
+  const componentHeight = 72;
   const [isClicked, setIsClicked] = useState(false);
+  const journeyRef = useRef(null);
+  const lineRef = useRef(null);
+
+  useEffect(() => {
+    const journeyElement = journeyRef.current;
+    const lineElement = lineRef.current;
+
+    if (journeyElement && lineElement) {
+      const height = journeyElement.clientHeight;
+      lineElement.style.height = `${height}px`;
+    }
+  });
+
   const handleClick = () => {
-    setIsClicked(true);
+    setIsClicked(!isClicked);
   };
+
   return (
-    <div
-      id="a journey"
-      className={`flex flex-row items-center ${
-        isClicked ? `h-auto` : `min-h-${componentHeight}`
-      }`}
-    >
+    <div ref={journeyRef} className={`flex flex-row items-center`}>
       <div
         id="duration"
-        className="flex flex-col -rotate-90 text-accent text-base md:text-xl min-w-fit max-h-min"
+        className="flex flex-col -rotate-90 text-accent text-center items-center whitespace-nowrap text-sm md:text-xl md:w-24 w-8"
       >
         {/* TODO: calendar logo */}
         {duration}
       </div>
-      <div className="items-center justify-center">
-        <div className="flex h-4 w-4 bg-accent rounded-full justify-center items-center">
+      <div id="timeline-line" className="flex items-center justify-center">
+        <div
+          id="big-circle"
+          className="flex max-h-3 w-3 md:max-h-4 md:w-4 bg-accent rounded-full justify-center items-center"
+        >
           <div
-            className={`flex min-h-max h-${componentHeight} w-1 bg-accent items-center justify-center`}
+            ref={lineRef}
+            id="line"
+            className={`flex w-0.5 md:w-1 bg-accent items-center justify-center`}
           >
-            <div className="flex h-1 w-1 bg-background rounded-full items-center"></div>
+            <div
+              id="small-circle"
+              className="flex h-0.5 w-0.5 md:h-1 md:w-1 bg-background rounded-full items-center"
+            ></div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col mx-4 md:mx-8 md:p-8">
+      <div
+        id="journey-point-info"
+        className="flex flex-col mx-4 md:mx-12 my-4 md:my-8"
+      >
         <h2 className="heading-2 py-1 md:py-2">{organisation}</h2>
         <h2 className="heading-2 font-light italic py-1 md:py-2">{title}</h2>
         <p
           className={`paragraph-sm overflow-hidden py-1 md:py-2 ${
-            isClicked ? "" : "max-h-48"
+            isClicked ? "max-h-fit" : "max-h-48"
           }`}
+          onClick={handleClick}
         >
           {smallBody}
         </p>
--- prateek/src/components/JourneySection.js
+++ prateek/src/components/JourneySection.js
@@ -5,14 +5,14 @@ function JourneySection({ className }) {
   return (
     <section
       id="journey"
-      className={`flex flex-col mx-12 md:mx-48 md:p-4 ${className}`}
+      className={`flex flex-col mx-6 md:mx-24 md:p-4 ${className}`}
     >
-      <h1 className="flex heading-1 my-16">My Journey</h1>
+      <h1 className="flex heading-1 my-16 mx-6 md:mx-24">My Journey</h1>
       <JourneyPoint
         duration="08/21 - present"
         organisation="Skit.ai"
         title="Software Developer"
-        smallBody="Lorem ipsum dolor sit amet consectetur. Feugiat elit consectetur scelerisque dui. Eu mollis quis purus eu condimentum lacinia pharetra feugiat vitae. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed. Augue viverra senectus nunc. Feugiat elit consectetur scelerisque dui. Eu mollis quis purus eu condimentum lacinia pharetra feugiat vitae. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed.Feugiat elit consectetur scelerisque dui. Eu mollis quis purus eu condimentum lacinia pharetra feugiat vitae. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed."
+        smallBody="Lorem ipsum dolor sit amet consectetur. Feugiat elit consectetur scelerisque dui. Eu mollis quis purus eu condimentum lacinia pharetra feugiat vitae. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed. Augue viverra senectus nunc. Feugiat elit consectetur scelerisque dui. Eu mollis quis purus eu condimentum lacinia pharetra feugiat vitae. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed.Feugiat elit consectetur scelerisque dui. Eu mollis quis purus eu condimentum lacinia pharetra feugiat vitae. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed. Cum elementum aliquam sed amet mauris phasellus aliquam leo. Lectus fermentum sollicitudin eget amet. Mus tincidunt dolor blandit sem semper consequat urna. Malesuada arcu orci id faucibus metus placerat purus. Ipsum ac neque malesuada aliquet sed."
       />
       <JourneyPoint
         duration="02/21 - 05/21"
