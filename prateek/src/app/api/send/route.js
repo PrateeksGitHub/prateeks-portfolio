6fb069acb8e496f51f7e12a966d2b61e760b0fe6
smolPrateek <prateek.agrawal@skit.ai>
2023-06-30 19:38:37 +0530

    removed Resend SDK usage, and made it an edge function

--- prateek/src/app/api/send/route.js
+++ prateek/src/app/api/send/route.js
@@ -1,33 +1,48 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
 export async function POST(request) {
   try {
-    const RESEND_API_KEY = process.env.RESEND_API_KEY;
-    const resend = new Resend(RESEND_API_KEY);
     console.log(request.body);
     const requestData = await request.json(); // Await the JSON data
     const { name, email, phone, message } = requestData;
     const confirmationEmailBody = `Thanks for getting in touch! I've received your message and will respond shortly.\nFollowing is your form submission summary:\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`;
     console.log(
-      `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID} using API Key ${process.env.RESEND_API_KEY}`
+      `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID}`
     );
-    const confirmationEmailResponse = await resend.emails.send({
-      from: process.env.SENDER_EMAIL_ID,
-      to: email,
-      bcc: process.env.MY_EMAIL_ID,
-      subject: "Thank you for contacting me",
-      text: confirmationEmailBody,
-      tags: [
-        {
-          name: "category",
-          value: "confirmation",
+    const confirmationEmailResponse = await fetch(
+      "https://api.resend.com/emails",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${RESEND_API_KEY}`,
         },
-      ],
-    });
-    console.log(confirmationEmailResponse);
+        body: JSON.stringify({
+          from: `Prateek <${process.env.SENDER_EMAIL_ID}>`,
+          to: email,
+          bcc: process.env.MY_EMAIL_ID,
+          subject: "Thank you for contacting me",
+          text: confirmationEmailBody,
+          tags: [
+            {
+              name: "category",
+              value: "confirmation",
+            },
+          ],
+        }),
+      }
+    );
 
-    return NextResponse.json(confirmationEmailResponse);
+    if (confirmationEmailResponse.ok) {
+      const data = await confirmationEmailResponse.json();
+      console.log(data);
+      return NextResponse.json(data);
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error });
