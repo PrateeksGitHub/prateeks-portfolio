e545da138ae69c4030ec47c686ec944d78ca2e6a
Prateek Agrawal <88235849+PrateeksGitHub@users.noreply.github.com>
2023-06-30 21:48:20 +0530

    Fixed emails in production build (#7)
    * trying to fetch RESEND_API_KEY inside the post function
    
    * log added for debugging
    
    * something awful
    
    * did something less awful
    
    * removed Resend SDK usage, and made it an edge function
    
    * idek
    
    * i reallyyy don't know
    
    * shrugs
    
    * stupid
    
    * updated api response
    
    ---------
    
    Co-authored-by: smolPrateek <prateek.agrawal@skit.ai>

--- prateek/src/app/api/send/route.js
+++ prateek/src/app/api/send/route.js
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
 export async function POST(request) {
   try {
@@ -10,24 +12,41 @@ export async function POST(request) {
     const { name, email, phone, message } = requestData;
     const confirmationEmailBody = `Thanks for getting in touch! I've received your message and will respond shortly.\nFollowing is your form submission summary:\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`;
     console.log(
-      `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID}`
+      `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID}.`
     );
-    const confirmationEmailResponse = await resend.emails.send({
-      from: process.env.SENDER_EMAIL_ID,
-      to: email,
-      bcc: process.env.MY_EMAIL_ID,
-      subject: "Thank you for contacting me",
-      text: confirmationEmailBody,
-      tags: [
-        {
-          name: "category",
-          value: "confirmation",
+    const confirmationEmailResponse = await fetch(
+      "https://api.resend.com/emails",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${RESEND_API_KEY}`,
         },
-      ],
-    });
-    console.log(confirmationEmailResponse);
+        body: JSON.stringify({
+          from: `Prateek <${process.env.SENDER_EMAIL_ID}>`,
+          to: email,
+          bcc: process.env.MY_EMAIL_ID,
+          subject: "Thank you for contacting me",
+          text: confirmationEmailBody,
+          tags: [
+            {
+              name: "category",
+              value: "confirmation",
+            },
+          ],
+        }),
+      }
+    );
+    // const json_res = await confirmationEmailResponse.json();
+    // console.log(json_res);
+    const data = confirmationEmailResponse.statusText;
+    console.log(data);
 
-    return NextResponse.json(confirmationEmailResponse);
+    if (confirmationEmailResponse.status == 200) {
+      return NextResponse.json({ success: true });
+    } else {
+      return NextResponse.json({ success: false });
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error });
