def93069034f8053a41defca276fa74ad3ded8a5
smolPrateek <prateek.agrawal@skit.ai>
2023-06-30 10:09:23 +0530

    trying to fetch RESEND_API_KEY inside the post function

--- prateek/src/app/api/send/route.js
+++ prateek/src/app/api/send/route.js
@@ -7,6 +7,7 @@ const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
 export async function POST(request) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     console.log(request.body);
     const requestData = await request.json(); // Await the JSON data
     const { name, email, phone, message } = requestData;
