d259617e1c068b2e2ead47006399480aba3882bd
smolPrateek <prateek.agrawal@skit.ai>
2023-06-30 19:14:44 +0530

    something awful

--- prateek/src/app/api/send/route.js
+++ prateek/src/app/api/send/route.js
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 
 export async function POST(request) {
   try {
-    const resend = new Resend(process.env.RESEND_API_KEY);
+    const resend = new Resend("re_SRf4duoN_HYjSEk4oFnN1JzBT2BKxmsz8");
     console.log(request.body);
     const requestData = await request.json(); // Await the JSON data
     const { name, email, phone, message } = requestData;
