1cd1293dcad92e7b8e25869799c7b87f1db5ce63
Prateek Agrawal <88235849+PrateeksGitHub@users.noreply.github.com>
2023-07-06 22:39:00 +0530

    Update route.js
    merge conflict fixes

--- prateek/src/app/api/send/route.js
+++ prateek/src/app/api/send/route.js
@@ -12,19 +12,7 @@ export async function POST(request) {
     const { name, email, phone, message } = requestData;
     const confirmationEmailBody = `Thanks for getting in touch! I've received your message and will respond shortly.\nFollowing is your form submission summary:\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`;
     console.log(
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID}.`
-=======
-      `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID} using API Key ${RESEND_API_KEY}`
->>>>>>> c068a8b (idek)
-=======
-      `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID} using API Key ${RESEND_API_KEY}.`
->>>>>>> 49ef094 (i reallyyy don't know)
-=======
-      `Sending email from ${process.env.SENDER_EMAIL_ID} to ${email} and ${process.env.MY_EMAIL_ID}.`
->>>>>>> 68eb766 (updated api response)
     );
     const confirmationEmailResponse = await fetch(
       "https://api.resend.com/emails",
@@ -50,8 +38,6 @@ export async function POST(request) {
       }
     );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     if (confirmationEmailResponse.ok) {
       console.log("Confirmation email sent successfully.");
       return NextResponse.json({ success: true });
@@ -60,22 +46,6 @@ export async function POST(request) {
       return NextResponse.error("Failed to send confirmation email.", {
         status: 500,
       });
-=======
-    if (confirmationEmailResponse.status == 200) {
-      return NextResponse.json({ success: true });
-    } else {
-      return NextResponse.json({ success: false });
->>>>>>> 68eb766 (updated api response)
-=======
-    if (confirmationEmailResponse.ok) {
-      console.log("Confirmation email sent successfully.");
-      return NextResponse.json({ success: true });
-    } else {
-      console.log("Failed to send confirmation email.");
-      return NextResponse.error("Failed to send confirmation email.", {
-        status: 500,
-      });
->>>>>>> c012fe6 (added validation and succes and failure message to the contact me form)
     }
   } catch (error) {
     console.error(error);
