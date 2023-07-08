7a2dedf21cd53ecb6f9681d841895b05af24b843
Prateek Agrawal <88235849+PrateeksGitHub@users.noreply.github.com>
2023-07-08 14:35:00 +0530

    minor about me section update (#10)
    * minor about me section update
    
    * updated autofill button for safari
    
    ---------
    
    Co-authored-by: smolPrateek <prateek.agrawal@skit.ai>

--- prateek/src/components/AboutMe.js
+++ prateek/src/components/AboutMe.js
@@ -7,13 +7,13 @@ function AboutMe({ className }) {
     <section id="about" className={`flex flex-col justify-center ${className}`}>
       <h1 className="font-bold text-4xl mb-8">About Me</h1>
       <p className="text-primary text-xl leading-relaxed">
-        As a FullStack Developer with 2 years of experience at Skit.ai, I have
-        honed my craft in designing, building, and maintaining production grade
-        software & data pipelines at scale. 👨‍💻 I am a beginner at UI-UX Design
-        🎨 and Music Production. 🎵 I pride myself on being an effective
-        communicator and also am passionate about Sustainability. 🌏 When Im not
-        being an engineer, I like to play Valorant 🎮 , take walks listening to
-        audiobooks & podcasts. Also, extremely rarely, I workout.
+        I'm a FullStack Developer with 2 years of experience at Skit.ai. My
+        focus is on designing and building reliable software solutions and
+        managing data pipelines at scale. 👨‍💻 I'm also dipping my toes into UI-UX
+        Design 🎨 and Music Production to expand my skills. 🎵 I value effective
+        communication and have a keen interest in Sustainability. 🌏 During my
+        downtime, I like playing Valorant 🎮, going for walks while listening to
+        audiobooks and podcasts, and extremely rarely, I squeeze in a workout.
       </p>
     </section>
   );
